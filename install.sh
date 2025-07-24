17a9c81512dec634769a152d58c1b9d26e8c3e5f
matzzcode <matzzcode@gmail.com>
2025-07-24 15:50:04 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,9 @@
+cd /var/www/pterodactyl/public
+
+wget https://files.phpmyadmin.net/phpMyAdmin/5.2.2/phpMyAdmin-5.2.2-all-languages.zip
+
+unzip phpMyAdmin-5.2.2-all-languages.zip
+
+rm phpMyAdmin-5.2.2-all-languages.zip
+
+mv phpMyAdmin-5.2.2-all-languages phpmyadmin
